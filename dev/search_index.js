8cf8c87ffee8f393c53bfc276a0fe58c8f776785
Documenter.jl <documenter@juliadocs.github.io>
2023-01-09 18:36:32 +0000

    build based on f0ad8ea

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"RegularizedCovarianceMatrices.CovarianceMatrixEstimator\nRegularizedCovarianceMatrices.EmpiricalCovarianceMatrix(n, d)\nRegularizedCovarianceMatrices.ShrunkCovarianceMatrix\nRegularizedCovarianceMatrices.OASCovarianceMatrix\nRegularizedCovarianceMatrices.LedoitWolfCovarianceMatrix\nRegularizedCovarianceMatrices.fit()\nRegularizedCovarianceMatrices.fit!()","category":"page"}]
+}
