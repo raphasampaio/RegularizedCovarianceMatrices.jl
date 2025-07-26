19f3a14b15e663b1d3024c657376fb01aabd1312
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-26 08:13:51 -0300

    Change to juliaup

--- revise/revise.bat
+++ revise/revise.bat
@@ -2,4 +2,4 @@
 
 SET BASEPATH=%~dp0
 
-CALL "%JULIA_194%" --project=%BASEPATH% --interactive --load=%BASEPATH%\revise.jl
+CALL julia +1.11 --project=%BASEPATH% --interactive --load=%BASEPATH%\revise.jl
